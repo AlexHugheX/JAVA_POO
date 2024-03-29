c1003414b77721334196731d5af1adc8dcc6d47a
AlexHugheX <manolounu@gmail.com>
2024-03-29 14:17:30 -0500

    MACF/feat: Create
    Aprendi a sumar, concatenar y pintar en consola.

---
+++ Code/Hello_World/Hello_World.java
@@ -0,0 +1,10 @@
+package Code.Hello_World;
+
+public class Hello_World {
+    public static void main(String[] args) {
+        System.out.print(1 + 2);
+        System.out.println(3 + "4");
+        System.out.println(5 * (8 - 3));
+        System.out.println("Hello" + ", World!");
+    }
+}
